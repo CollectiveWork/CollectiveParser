579fd10dafaf9c4df4a0d575f026b848df60d41b
Butnaru Andrei-Madalin <butnaruandreimadalin@gmail.com>
2014-02-25 22:56:42 +0200

    Finished CharacterSet defines.

--- src/CharacterSet.java
+++ src/CharacterSet.java
@@ -1,10 +1,48 @@
-import com.sun.deploy.util.ArrayUtil;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 /**
  * Created by Cristi on 2/25/14.
  */
 public class CharacterSet {
-    public static String[] lower_case = "a|b|c|d|e|f|g|h|i|j|k|l|m|n|o|p|q|r|s|t|u|v|w|x|y|z".split("|");
-    public static String[] upper_case = "A|B|C|D|E|F|G|H|I|J|K|L|M|N|O|P|Q|R|S|T|U|V|W|X|Y|Z".split("|");
+    public static String[] lower_case;
+    public static String[] upper_case;
+    public static String[] alphabetic;
+    public static String[] numeric;
+    public static String[] alphanumeric;
+    public static String[] special;
+    public static String[] graphic;
 
+
+    public static void readCharacterSet(String filePath){
+        String lower_case_raw = "";
+        String upper_case_raw = "";
+        String numeric_raw = "";
+        String special_raw = "";
+        try {
+            FileReader fileReader = new FileReader(filePath);
+            BufferedReader textReader = new BufferedReader(fileReader);
+
+            lower_case_raw = textReader.readLine();
+            upper_case_raw = textReader.readLine();
+            numeric_raw = textReader.readLine();
+            special_raw = textReader.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        String alphabetic_raw = lower_case_raw + " " + upper_case_raw;
+        String alphanumeric_raw = alphabetic_raw + " " + numeric_raw;
+        String graphic_raw = alphanumeric_raw + " " + special_raw;
+
+        lower_case = lower_case_raw.split(" ");
+        upper_case = upper_case_raw.split(" ");
+        alphabetic = alphabetic_raw.split(" ");
+        numeric = numeric_raw.split(" ");
+        alphanumeric = alphanumeric_raw.split(" ");
+        special = special_raw.split(" ");
+        graphic = graphic_raw.split(" ");
+
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -8,9 +8,15 @@ public class Main {
     public static void main(String[] args) {
         try {
             System.out.println( Reader.readFile("resources//code.txt", null));
+            CharacterSet.readCharacterSet("resources//javaCharset.txt");
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        for (int i = 0; i < CharacterSet.graphic.length; i++) {
+            System.out.print(CharacterSet.graphic[i]);
+        }
+
 
     }
 }
