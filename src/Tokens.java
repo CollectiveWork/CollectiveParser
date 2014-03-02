a5cc05d9bebeab88bfe1ccc7de6d44a918d07606
Root <earthro.ot46@yahoo.com>
2014-03-02 17:15:35 +0200

    Add Operators, start Literals

--- src/Tokens.java
+++ src/Tokens.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 public class Tokens {
     public static ArrayList<ArrayList<String>> tokens = new ArrayList<ArrayList<String>>();
 
-    public static ArrayList<String> identifiers = new ArrayList<String>();
+   // public static ArrayList<String> identifiers = new ArrayList<String>();
     public static ArrayList<String> keywords = new ArrayList<String>();
     public static ArrayList<String> separators = new ArrayList<String>();
     public static ArrayList<String> operators = new ArrayList<String>();
@@ -51,9 +51,31 @@ public class Tokens {
             separators.add(separator);
         }
     }
+    public static void readOperators(){
+        String[] operators_new = Reader.readFile("resources//java//operators.txt", null).split(" ");
+
+        for (String operator : operators_new) {
+            operators.add(operator);
+        }
+
+    }
 
     public static void readTokens() {
         readKeywords();
+        readSeparators();
+        readOperators();
+    }
+
+
+    private static class Literals{
+
+        public static ArrayList<String> integer_literal = new ArrayList<String>();
+        public static ArrayList<String> floating_point_literal = new ArrayList<String>();
+        public static ArrayList<String> boolean_literal = new ArrayList<String>();
+        public static ArrayList<String> character_literal = new ArrayList<String>();
+        public static ArrayList<String> string_literal = new ArrayList<String>();
+        public static ArrayList<String> null_literal = new ArrayList<String>();
+
 
     }
 }
